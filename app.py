109da19ed9a5d4bdedfbb4bd0a75322c445ba599
Jilian <maurelji@etu.univ-grenoble-alpes.fr>
2022-05-19 21:56:17 +0200

    BDD V2

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ Création: maurelji, le 13/05/2022
 
 
 # Imports
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, current_app
 
 # Routes
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['DATABASE'] = '/bdd/bdd_v2.sqlite'
+
 
 @app.route("/")
 def home():
---
+++ tests/db_connection_test.py
@@ -0,0 +1,13 @@
+
+import sqlite3
+from flask import current_app, g
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(current_app.config['DATABASE'])
+        g.db.row_factory = sqlite3.Row
+        current_app.logger.info(f'Connexion à la BDD')
+    return g.db
+
+get_db()
+

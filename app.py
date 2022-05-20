98ca2f85dcafac683b8b9cd02f0b897538c07873
cosmem <cosmem@etu.univ-grenoble-alpes.fr>
2022-05-20 11:59:17 +0200

    ajout fonction de login/logout

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Création: maurelji, le 13/05/2022
 
 
 # Imports
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 import util
 
 # Routes
@@ -19,6 +19,28 @@ app.teardown_appcontext(util.close_db)
 @app.route("/")
 def home():
     return render_template("home.html", vacataires=util.get_data())
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    login = request.form.get('login')
+    password = request.form.get('password')
+    if util.check_login(login, password):
+        flash("Connexion réussie")
+        session['login'] = login
+        return redirect(url_for('home'))
+    flash("Identifiants invalides")
+    return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    if 'login' in session:
+        session.pop('login')
+        flash('Déconnexion réussie')
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import current_app, g
+import bcrypt
 
 
 def get_db():
@@ -22,3 +23,27 @@ def get_data():
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM vacataires""")
     return cursor.fetchall()
+
+
+def get_user(user):
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("""SELECT login, mdp, role FROM comptes
+                          WHERE login = :login""",
+                   {'login': user})
+    user = cursor.fetchone()
+    return user
+
+
+def password_hash(password):
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode("utf-8")
+
+
+def password_verify(password, hash):
+    return bcrypt.checkpw(password.encode('utf-8'),
+                          hash.encode('utf-8') if isinstance(hash, str) else hash)
+
+
+def check_login(login, password):
+    user = get_user(login)
+    return user is not None and password_verify(password, user['mdp'])
